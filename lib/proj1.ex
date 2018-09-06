f1a15265dd73f239140bbfb7e1175dbc93c78cf4
Truc D T Nguyen <truc.ndtt@gmail.com>
2018-09-06 12:35:17 -0400

    Second version

--- lib/proj1.ex
+++ lib/proj1.ex
@@ -1,17 +1,36 @@
 defmodule Proj1 do
-  def findPerfectSquare(n, k) do
-    for x <- 1..n do
-      spawn(Worker, :squareSumList, [self(), x, k])
-#      Worker.squareSumList(self(), x, k)
+  def startBoss(n, k, noActors) do
+    workUnit = div(n, noActors)
+    remainder = rem(n, noActors)
+    runActors = if (workUnit == 0), do: remainder, else: noActors
+
+    ### Calculate and assisn work units to each actor
+    for i <- 1..runActors do
+      start = findStartIndex(i, remainder, workUnit)
+
+      if i <= remainder do
+        spawn(Worker, :findPerfectSquare, [self(), start, workUnit + 1, k])
+      else
+        spawn(Worker, :findPerfectSquare, [self(), start, workUnit, k])
+      end
     end
 
-    waitForWorkers(0, n)
+    ### Wait until all actors finished
+    waitForWorkers(0, runActors)
   end
 
-  def waitForWorkers(count, n) do
-    if count < n do
+  def findStartIndex(proc, remainder, workUnit) do
+    if proc <= remainder + 1 do
+      (proc - 1) * (workUnit + 1) + 1
+    else
+      remainder * (workUnit + 1) + (proc - 1 - remainder) * workUnit + 1
+    end
+  end
+
+  def waitForWorkers(count, noActors) do
+    if count < noActors do
       receive do
-        :finish -> waitForWorkers(count + 1, n)
+        :finish -> waitForWorkers(count + 1, noActors)
       end
     end
   end
@@ -22,13 +41,18 @@ defmodule Worker do
     n * (n + 1) * (2 * n + 1) / 6
   end
 
-  def squareSumList(boss, start, k) do
-    sum = sumOfSquares(start + k - 1) - sumOfSquares(start - 1)
+  def checkSumToSquare(first, k) do
+    sum = sumOfSquares(first + k - 1) - sumOfSquares(first - 1)
 
     rootTrunc = trunc(:math.sqrt(sum))
+    rootTrunc * rootTrunc == sum
+  end
 
-    if rootTrunc * rootTrunc == sum do
-      IO.inspect(start)
+  def findPerfectSquare(boss, start, workUnit, k) do
+    for i <- start..start + workUnit - 1 do
+      if checkSumToSquare(i, k) do
+        IO.inspect(i)
+      end
     end
 
     send(boss, :finish)
--- proj1.exs
+++ proj1.exs
@@ -1,2 +1,5 @@
+noActors = 8
+
 [n, k] = Enum.map(System.argv(), fn x -> String.to_integer(x)  end)
-Proj1.findPerfectSquare(n, k)
+
+Proj1.startBoss(n, k, noActors)
