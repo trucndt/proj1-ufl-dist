ea1ca06efdbac66c273e3691ebd2e05b5ea27acf
Truc D T Nguyen <truc.ndtt@gmail.com>
2018-09-10 14:12:32 -0400

    add comments to script

--- proj1.exs
+++ proj1.exs
@@ -1,5 +1,7 @@
-noActors = 8
+noActors = 8 # desired number of actors
 
+# Parse command line input
 [n, k] = Enum.map(System.argv(), fn x -> String.to_integer(x)  end)
 
+# Start program
 Proj1.startBoss(n, k, noActors)
